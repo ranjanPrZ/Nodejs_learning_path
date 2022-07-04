ce1c967681c72617bbf86dc02bba23294b817e30
@ranjanRe96 <ranjan.kumar@re-digital.io>
2022-07-04 12:40:59 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express()
+const port = 3000
+const path = require('path')
+// console.log(app)
+// Confirm server is getting request.
+// app.use((req, res) => {
+//   console.log('We got a new request!!')
+//   res.send("<h1>This is my webpage</h1>")
+// })
+
+
+// Set view engine with EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+
+// First route to check whether server running.
+app.listen(port, ()=>{
+  console.log("Listing on port 3000!")
+})
+
+app.get('/cats', (req, res)=> {
+  const cats = ['Blue', 'Red', 'Yello', 'Brown']
+  res.render('cats', {cats})
+})
+
+app.get('/dogs', (req, res)=> {
+  res.send('this is the sound from Dog, Woof!!');
+})
+
+app.get('/r/:subreddit/:three', (req, res) => {
+  const {subreddit, secondId} = req.params;
+  res.send(`<h1>Browsing the subreddit ${subreddit} : ${secondId}`);
+
+})
+
+app.get('/books', (req, res) => {
+  res.send('This is the books list')
+})
+
+app.get('/search', (req, res) => {
+  const q = req.query;
+// To do understand why q gets undefined.
+
+  if (!q) {
+    console.log('Nothing found if nothing searched!')
+  }
+  res.send(`<h1>Search results for: ${q} </h1>`)
+})
+
+app.get('/home', (req, res) => {
+  const route_paths = ['/search', '/cats', '/dogs', '/books', '/r/subreddit']
+  res.render('home', {route_paths})
+})
+
+app.get('/rand', (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('random', {num: num})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.render('subreddit', {subreddit});
+})
+// This matches every single request.
+// app.get('*', (req, res) => {
+//   res.send('I dont know the path')
+// })
